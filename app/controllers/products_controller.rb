9753c8b2c1814039d3f1b42600973f08c4b3d286
ameliarider <mimirider89@gmail.com>
2025-06-09 12:00:05 -0700

    Adding products controller CRUD actions

--- app/controllers/products_controller.rb
+++ app/controllers/products_controller.rb
@@ -1,16 +1,46 @@
 class ProductsController < ApplicationController
-  def index
+def index
+    @products = Product.all
+    render :index
   end
 
   def show
+    @product = Product.find(params["id"])
+    render :show
   end
 
   def create
+    @product = Product.create(
+      name: params["name"],
+      shop_owner: params["shop_owner"],
+      price: params["price"],
+    )
+
+    if @product.valid?
+      render :show
+    else
+      render json: { errors: @product.errors.full_messages }
+    end
   end
 
   def update
+    @product = Product.find(params["id"])
+    @product.update(
+      name: params["name"] || @product.name,
+      shop_owner: params["shop_owner"] || @product.shop_owner,
+      price: params["price"] || @product.price
+    )
+    if @product.valid?
+      render :show
+    else
+      render json: { errors: @product.errors.full_messages }
+    end
   end
 
   def destroy
+    @product = Product.find(params["id"])
+    @product.destroy
+
+    render json: { message: "Product deleted" }
   end
 end
